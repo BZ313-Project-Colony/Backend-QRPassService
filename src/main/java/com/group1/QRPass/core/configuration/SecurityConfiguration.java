70a4d911f23a51520b2e12fa1c9670b7000b53d2
Batuhan Erol <bataer57@gmail.com>
2023-11-19 00:02:15 +0300

    Sprint 2 | Security Config modified

--- src/main/java/com/group1/QRPass/core/configuration/SecurityConfiguration.java
+++ src/main/java/com/group1/QRPass/core/configuration/SecurityConfiguration.java
@@ -62,6 +62,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/v1/auth/**").permitAll();
                     auth.requestMatchers("/hello").hasAuthority("ROLE_ADMIN");
+                    auth.requestMatchers("/v1/events").hasAuthority("ROLE_ADMIN");
                     auth.anyRequest().authenticated();
                 });
         httpSecurity.oauth2ResourceServer( (oauth2) -> oauth2.jwt(jwtConfigurer ->
